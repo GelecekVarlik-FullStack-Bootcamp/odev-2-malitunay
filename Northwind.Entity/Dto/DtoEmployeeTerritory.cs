ca49d9490e0db5ad6004325213f12ba38000cce7
malitunay <malitunay1@gmail.com>
2022-04-21 19:16:56 +0300

    hafta2-2 commit

---
+++ Northwind.Entity/Dto/DtoEmployeeTerritory.cs
@@ -0,0 +1,15 @@
+﻿using Northwind.Entity.Base;
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Northwind.Entity.Dto
+{
+    public class DtoEmployeeTerritory : DtoBase
+    {
+        public int EmployeeId { get; set; }
+        public string TerritoryId { get; set; }
+
+    }
+}
